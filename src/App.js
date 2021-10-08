2ec43574f03cb48fb6ef3bca573f48b6e5540177
Romano Livi <romano_livi@outlook.com>
2021-10-07 23:49:25 -0400

    Fixed log in and sign up features

--- src/App.js
+++ src/App.js
@@ -1,52 +1,71 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import { createBrowserHistory } from 'history';
-import { BrowserRouter as Link, Router, Route} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Nav, Navbar, NavbarBrand, NavDropdown } from 'react-bootstrap';
+import { BrowserRouter, Route, NavLink, Switch} from 'react-router-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
+import { Nav, Navbar, NavbarBrand, Dropdown, NavItem, DropdownItem } from 'reactstrap';
 import Home from './components/Home';
 import StockSearch from './components/StockSearch';
 import MarketNews from './components/MarketNews/MarketNews';
 import Trends from './components/Trends';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 
-function App() {
+
+
+const defaultHistory = createBrowserHistory();
+
+const App = ( props, {history = defaultHistory}) => {
+
+  const loggedIn = props.loggedIn
 
   return (
-    <div>
-      <div className="jumbotron">
-        <Navbar bg="dark" variant="dark" sticky="top">
-          <Navbar.Brand >
-            <img src="https://cdn-images-1.medium.com/max/1200/1*fmJW9a3HTPWAsCKjnz2FRQ.png" height="30px" width="30px"/>{' '}
-            Stock App
-          </Navbar.Brand>
-
-          <Nav>
-            <NavDropdown title="Stocks">
-              <NavDropdown.Item href="/search">Stock Search</NavDropdown.Item>
-              <NavDropdown.Item href="/charts">Charts</NavDropdown.Item>
-              <NavDropdown.Item href="/options">Options</NavDropdown.Item>
-            </NavDropdown>
-            
-            <Nav.Link href="/trends">Trends</Nav.Link>
-            <Nav.Link href="/news">Market News</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-
-
-        </Navbar>
-      </div>
+    <BrowserRouter history={history}>
       <div>
-        <Route exact path={'/'} component={Home} />
-        <Route exact path={'/search'} component={StockSearch} />
-        <Route exact path={'/news'} component={MarketNews} />
-        <Route exact path={'/trends'} component={Trends} />
-      </div>
+        <div className="jumbotron">
+          { loggedIn && <Navbar bg="dark" variant="dark" sticky="top">
+            <NavbarBrand className="home-logo" tag={NavLink} to={'/'}>
+              <img src="https://cdn-images-1.medium.com/max/1200/1*fmJW9a3HTPWAsCKjnz2FRQ.png" height="30px" width="30px"/>{' '}
+              Stock App
+            </NavbarBrand>
+            
+              <Nav className="me-auto">
+                
+                <NavItem>
+                  <NavLink className="nav-link" id="navl" to={'/trends'}>Trends</NavLink>
+                </NavItem>
 
-      <div className="content">
-        
+                <NavItem>
+                  <NavLink className="nav-link" id="navl" to={'/news'}>Market News</NavLink>
+                </NavItem>
+
+                <NavItem>
+                  <NavLink className="nav-link" id="navl" to={'/contact'}>Contact</NavLink>
+                </NavItem>
+              </Nav> 
+          
+          </Navbar> }
+        </div>
+        <div>
+          <Switch>  
+            <Route exact path={'/login'} component={Login} />
+            <Route exact path={'/'} component={Home} />
+            <Route exact path={'/search'} component={StockSearch} />
+            <Route exact path={'/news'}component={MarketNews} />
+            <Route exact path={'/trends'} component={Trends} />
+            <Route exact path={'/sign'} component={SignUp} />
+          </Switch>
+        </div>
+
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
-export default App;
+const mapStateToProps = ({loggedIn}) => {
+  return { loggedIn }
+}
+
+export default connect(mapStateToProps)(App);
